534abfa24c4dcd149f9dc7f45561da3e57e32d3d
Gabrielle Vargas Dornelles <vargasdornellesgabrielle@gmail.com>
2026-09-25 15:54:10 -0300

    estrutura_condicional

--- estrutura_condicional/src/Main.java
+++ estrutura_condicional/src/Main.java
@@ -77,7 +77,25 @@ public class Main {
         int y = teclado.nextInt();
         int z = teclado.nextInt();
 
-        
+        if (x > y && x > z) {
+            if (y > z) {
+                System.out.println(x + " " + y + " " + z);
+            } else {
+                System.out.println(x + " " + z + " " + y);
+            }
+        } else if (y > x && y > z) {
+            if (x > z) {
+                System.out.println(y + " " + x + " " + z);
+            } else {
+                System.out.println(y + " " + z + " " + x);
+            }
+        } else {
+            if (x > y) {
+                System.out.println(z + " " + x + " " + y);
+            } else {
+                System.out.println(z + " " + y + " " + x);
+            }
+        }
 
         }
     }
